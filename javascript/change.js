d92b038604b5f49d433250c29484e8e5143307fe
lanmingqun <www.lanmingqun@163.com>
2017-09-26 21:05:09 +0800

    666

---
+++ javascript/change.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017\9\18 0018.
+ */
+var mySwiper = new Swiper(".banner", {
+    direction: "vertical",
+    pagination: ".swiper-pagination"
+})
+
+/*
+var mySwiperT = new Swiper(".three", {
+    autoplay: 2000,
+    loop: true,
+    pagination: ".swiper-pagination",
+    paginationClickable: true,
+    effect: "flip"
+})*/
